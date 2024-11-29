51c6cc2af8930a0211296a73d9529784e985002d
bulbajs <93475048+bulbajs@users.noreply.github.com>
2024-11-30 00:35:23 +0300

    Delete d2_models/d2_models directory

--- d2_models/d2_models/news/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-admin.site.register(Post)
-admin.site.register(Category)
-admin.site.register(PostCategory)
-admin.site.register(Author)
-admin.site.register(Comment)
-
